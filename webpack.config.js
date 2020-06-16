cefed4afb22f2c01f68b4da2d591dbb0abe7b819
Aliona Krasovskaya <aliona.krasovskaya@finstek.com>
2020-06-17 01:57:15 +0300

    webpack dev server and HMR feature

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
-     app: './src/index.js',
-     print: './src/print.js',
+     app: './src/index.js'
    },
+   devtool: 'inline-source-map',
    output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+     contentBase: './dist',
+     hot: true
   },
   plugins: [
     new CleanWebpackPlugin(),
