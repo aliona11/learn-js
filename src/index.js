9bc6e7f813834468f7a5dc56f7a78aa2ed931312
Aliona Krasovskaya <aliona.krasovskaya@finstek.com>
2020-06-17 01:49:28 +0300

    asset setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Rectangle from './rectangle.svg';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  const mySvg = new Image();
+  mySvg.src = Rectangle;
+
+  element.appendChild(mySvg);
 
   return element;
 }
