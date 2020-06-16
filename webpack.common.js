7d763373f1c7948ab5d5c9a0f373127442c09557
Aliona Krasovskaya <aliona.krasovskaya@finstek.com>
2020-06-17 01:59:47 +0300

    configs for dev mode and prod mode, optimization(minifications), npm scripts (start, build).

--- webpack.common.js
+++ webpack.common.js
@@ -3,18 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
      app: './src/index.js'
-   },
-   devtool: 'inline-source-map',
+   },   
    output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-     contentBase: './dist',
-     hot: true
   },
   plugins: [
     new CleanWebpackPlugin(),
